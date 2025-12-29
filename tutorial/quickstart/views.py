9a96cb5406d2403a856ce23b0c6c0774fa1ea67b
Rajkumar <raj14kumarp@gmail.com>
2025-12-30 01:35:50 +0530

    added templates and static

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Question
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(request):
+    # latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    # context = {"latest_question_list": latest_question_list}
+    return render(request, "index.html")
